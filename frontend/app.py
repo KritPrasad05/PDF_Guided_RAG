6ce34a4644f61cd2deb5d74ff0d2cdb9ebffabc3
_Krit_Prasad_ <126701586+KritPrasad05@users.noreply.github.com>
2026-03-03 15:44:48 +0530

    Adding the Frontend Logic

---
+++ frontend/app.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import requests
+
+import re
+
+BACKEND_URL = "http://127.0.0.1:8001"
+
+st.title("Gemini PDF RAG System")
+
+uploaded_file = st.file_uploader("Upload PDF", type="pdf")
+
+if uploaded_file is not None:
+    files = {
+        "file": (
+            uploaded_file.name,
+            uploaded_file.getvalue(),
+            "application/pdf"
+        )
+    }
+
+    response = requests.post(
+        f"{BACKEND_URL}/upload",
+        files=files
+    )
+    st.write(response.text)
+question = st.text_input("Ask Question")
+
+if st.button("Ask"):
+    response = requests.get(
+        f"{BACKEND_URL}/ask",
+        params={"question": question}
+    )
+    answer = response.json()["answer"]
+
+    st.markdown("## Answer")
+
+    # detect LaTeX math
+    latex_blocks = re.findall(
+        r'\$\$(.*?)\$\$|\\\[(.*?)\\\]',
+        answer,
+        re.DOTALL
+    )
+
+    if latex_blocks:
+
+        st.markdown(answer)
+
+        for block in latex_blocks:
+
+            formula = block[0] or block[1]
+
+            st.latex(formula)
+
+    else:
+
+        st.markdown(answer)
